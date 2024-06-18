40d1d44625216f80e9150cdabe27a522e6044377
idnc_sk <me@idnc.sk>
2024-06-18 20:28:31 +0200

    sync

--- src/managers/role/lib/Role.js
+++ src/managers/role/lib/Role.js
@@ -1,28 +1,53 @@
-const EventEmitter = require('eventemitter2');
 
-class Role extends EventEmitter {
+/*
+https://www.npmjs.com/package/dockerode
+container.inspect(options) - Docker API Endpoint
+container.start(options) - Docker API Endpoint
+container.stop(options) - Docker API Endpoint
+container.pause(options) - Docker API Endpoint
+container.unpause(options) - Docker API Endpoint
+container.restart(options) - Docker API Endpoint
+container.kill(options) - Docker API Endpoint
+container.attach(options) - Docker API Endpoint
+container.remove(options) - Docker API Endpoint
+container.logs(options) - Docker API Endpoint
+container.stats(options) - Docker API Endpoint
+*/
 
-    constructor(options) {
-        super(options);
-        this.options = options;
+class Role {
 
-        this.name = options.name;
-        this.description = options.description;
-        this.version = options.version;
-        this.author = options.author;
-        this.backend = options.backend;
+    constructor(name, description) {
+        this.name = name;
+        this.description = description
     }
 
-    start() {}
+    getConfig() {}
 
-    stop() {}
+    inspect() { }
 
-    restart() {}
+    start() { }
+
+    stop() { }
+
+    pause() { }
+
+    unpause() { }
+
+    restart() { }
+
+    kill() { }
+
+    attach() { }
+
+    remove() { }
+
+    logs() { }
+
+    stats() { }
 
     status() {}
-
-    // initialize(options) {}
 
 }
 
 module.exports = Role;
+
